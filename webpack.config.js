ab2dc50c27c66cd18415bca11f2582393d653d8c
vladimirgorbunov <gvv_93@mail.ru>
2016-03-18 18:18:12 +0300

    dev framework

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ let ExtractTextPlugin = require('extract-text-webpack-plugin');
 let SvgStore = require('webpack-svgstore-plugin');
 let HandlebarsPlugin = require("handlebars-webpack-plugin");
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
     context: __dirname + "/app",
     entry: {
@@ -12,9 +14,9 @@ module.exports = {
     },
 
     output: {
-        path: __dirname + '/server',
+        path: NODE_ENV == 'development' ? __dirname + '/server' : __dirname + '/dist/local/assets',
         publicPath: '/',
-        filename: '[name].js'
+        filename: NODE_ENV == 'development' ? '[name].js' : './js/[name].js'
     },
 
     module: {
@@ -43,7 +45,7 @@ module.exports = {
     },
 
     plugins: [
-        new ExtractTextPlugin("./main.css", {allChunks: true, disable: process.env.NODE_ENV=="development"}),
+        new ExtractTextPlugin( NODE_ENV == 'development' ? "./main.css" : './css/main.css', {allChunks: true, disable: NODE_ENV == 'development'}),
         new SvgStore(path.join(__dirname + '/app/local/assets/images', 'svg', '**/*.svg'), path.join('./images', 'svg'), {
             name: '[hash].svg',
             chunk: 'app',
@@ -52,7 +54,7 @@ module.exports = {
         new HandlebarsPlugin({
             entry: path.join(process.cwd(), "app", "templates", "home.hbs"),
 
-            output: path.join(process.cwd(), "server", "index.html"),
+            output: NODE_ENV == 'development' ?  path.join(process.cwd(), "server", "index.html") : path.join(process.cwd(), "dist", "html", "index.html"),
             partials: [
                 path.join(process.cwd(), "app", "templates", "partials", "*", "*.hbs")
             ]
